d403b00082a24feaa8dc4680fc342e0538b92c10
RahulGade03 <rahulgade2004@gmail.com>
2025-02-26 14:32:23 +0530

    backend modification

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const app = require('./server');
+const { createServer } = require('@vercel/node');
 const port = 3000
 const dotenv = require('dotenv');
 dotenv.config();
@@ -57,3 +59,5 @@ app.post ('/saveOne', async (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+module.exports = createServer(app);
