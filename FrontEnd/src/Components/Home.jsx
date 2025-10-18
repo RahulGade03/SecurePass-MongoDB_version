051e2dc8c7c4578d64e00b8d8de31631dda37ae6
RahulGade03 <rahulgade2004@gmail.com>
2025-10-18 22:11:42 +0530

    added try catch

--- FrontEnd/src/Components/Home.jsx
+++ FrontEnd/src/Components/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
   const passRef = useRef()
 
   useEffect(() => {
-    async function run (){
+    async function run() {
       // console.log(user);
       let pass = await fetch(`https://securepassbackend.vercel.app/getData?emailId=${user.email}`)
       pass = await pass.json()
@@ -23,68 +23,72 @@ const Home = () => {
         setPasswords(pass)
       }
     }
-    run ()
+    run()
   }, [isAuthenticated])
 
 
 
   async function savePassword() {
-    if (!form.website) {
-      toast.error('Please enter url!', {
-        position: "top-right",
-        autoClose: 4000,
-        hideProgressBar: false,
-        closeOnClick: false,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-      });
-    }
-    if (!form.username) {
-      toast.error('Please enter username!', {
-        position: "top-right",
-        autoClose: 4000,
-        hideProgressBar: false,
-        closeOnClick: false,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-      });
-    }
-    if (!form.password) {
-      toast.error('Please enter password!', {
-        position: "top-right",
-        autoClose: 4000,
-        hideProgressBar: false,
-        closeOnClick: false,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-      });
-    }
-    if (form.password && form.website && form.username) {
-      const id = uuidv4()
-      const data = { ...form, id: id }
-      setPasswords([...passwords, data])
-      await fetch('https://securepassbackend.vercel.app/saveOne', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({...form, id: id})
-      });
-      setForm({ loginId: user.email, website: '', username: '', password: '', id: '' })
-      toast.info('Your password has been saved!', {
-        position: "top-right",
-        autoClose: 4000,
-        hideProgressBar: false,
-        closeOnClick: false,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-      });
+    try {
+      if (!form.website) {
+        toast.error('Please enter url!', {
+          position: "top-right",
+          autoClose: 4000,
+          hideProgressBar: false,
+          closeOnClick: false,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      }
+      if (!form.username) {
+        toast.error('Please enter username!', {
+          position: "top-right",
+          autoClose: 4000,
+          hideProgressBar: false,
+          closeOnClick: false,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      }
+      if (!form.password) {
+        toast.error('Please enter password!', {
+          position: "top-right",
+          autoClose: 4000,
+          hideProgressBar: false,
+          closeOnClick: false,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      }
+      if (form.password && form.website && form.username) {
+        const id = uuidv4()
+        const data = { ...form, id: id }
+        setPasswords([...passwords, data])
+        await fetch('https://securepassbackend.vercel.app/saveOne', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ ...form, id: id })
+        });
+        setForm({ loginId: user.email, website: '', username: '', password: '', id: '' })
+        toast.info('Your password has been saved!', {
+          position: "top-right",
+          autoClose: 4000,
+          hideProgressBar: false,
+          closeOnClick: false,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      }
+    } catch (error) {
+      console.log(error);
     }
   }
 
@@ -129,10 +133,10 @@ const Home = () => {
         {isAuthenticated && <button className='bg-green-500 transition-all ease-in-out duration-300 hover:bg-green-600 py-2 px-7 rounded-full flex gap-2 items-center justify-center cursor-pointer' onClick={savePassword}>
           <lord-icon src="https://cdn.lordicon.com/jgnvfzqg.json" trigger="hover"></lord-icon>
           <span className='font-bold text-2xl'>Save</span></button>}
-        {!isAuthenticated && <LoginButton/>}
+        {!isAuthenticated && <LoginButton />}
       </div>
       {passwords.length == 0 && <div className='text-center font-bold mt-10'>No saved passwords yet</div>}
-      {passwords.length > 0 && <Passwords passwords={passwords} setPasswords={setPasswords} form={form} setForm={setForm} webRef={webRef} userRef={userRef} passRef={passRef}/>}
+      {passwords.length > 0 && <Passwords passwords={passwords} setPasswords={setPasswords} form={form} setForm={setForm} webRef={webRef} userRef={userRef} passRef={passRef} />}
     </div>
   )
 }
